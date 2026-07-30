bf57d89e2d9805c202d0f02df8ed7da57a9a2aec
Emma Thomas <thomasem@wgc.school.nz>
2026-07-30 22:47:43 +0000

    Importing & Connecting to the Database

--- app.py
+++ app.py
@@ -3,11 +3,23 @@ import sqlite3
 from sqlite3 import Error
 
 app = Flask(__name__)
+DATABASE = "vendor_management_system.db"
+
+def create_connection(db_file):
+    """Creates a connnection to the database"""
+
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
 
 @app.route('/')
 def render_home():
     search_query = request.args.get('search')
     return render_template('index.html', search=search_query)
+
 
 if __name__ == "__main__":
     app.run()
